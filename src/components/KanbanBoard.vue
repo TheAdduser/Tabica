4b00486c99e0c5161f6cbca1ed4fd3145dcd2b8f
TheAdduser <EasyRider190@gmail.com>
2025-01-05 16:19:37 +0100

    add priority icons to tasks

--- src/components/KanbanBoard.vue
+++ src/components/KanbanBoard.vue
@@ -15,17 +15,16 @@ const errorMessage = ref('');
 const showModal = ref(false);
 const selectedTask = ref(null);
 
-
 const fetchData = async () => {
   try {
-    columns.value = await pb.collection('projects').getOne(props.projectId,{
+    const project = await pb.collection('projects').getOne(props.projectId, {
       expand: 'column.task',
-      sort: 'expand.column.columnOrder',
     });
 
+    columns.value = project.expand.column.sort((a, b) => a.columnOrder - b.columnOrder);
+    tasks.value = project.expand.column.flatMap(column => column.expand.task);
     console.log('Fetched columns:', columns.value);
-  }
-  catch (error) {
+  } catch (error) {
     errorMessage.value = 'Failed to fetch columns';
   }
 };
@@ -45,7 +44,7 @@ const handleTaskUpdated = () => {
 };
 
 watch(() => props.projectId, () => {
-  if (typeof(props.projectId) !== 'undefined') {
+  if (typeof props.projectId !== 'undefined') {
     fetchData();
   }
 });
@@ -53,33 +52,38 @@ watch(() => props.projectId, () => {
 </script>
 
 <template>
-    <div class="space-x-4 mt-4 max-w-[calc(100vw-2rem)] pl-5">
-       <div class="flex flex-row gap-6 min-h-[calc(100vh-2rem)] " v-if="columns.length!=0">
+  <div class="p-5">
+    <div v-if="errorMessage" class="p-2 text-red-600 bg-red-100 rounded">
+      {{ errorMessage }}
+    </div>
+    <div class="space-x-4 max-w-[calc(100vw)]">
+      <div class="flex gap-6 min-h-[calc(100vh-2rem)]" v-if="columns.length != 0">
         <div
-          v-for="column in columns.expand.column"
-          :key="column"
-          class="p-4 bg-gray-800 rounded-lg shadow-md min-w-64 max-w-64"
+          v-for="column in columns"
+          :key="column.id"
+          class="p-4 bg-gray-800 rounded-lg shadow-md min-w-44 max-w-44"
         >
           <h3 class="text-xl font-bold text-white">{{ column.name }}</h3>
           <div class="space-y-2 mt-4">
             <div
               v-for="task in column.expand.task"
-              :key="task"
-              class="p-2 bg-gray-700 rounded-lg text-white min-h-30 font-bold hover:cursor-pointer hover:bg-gray-600"
+              :key="task.id"
+              class="relative p-2 bg-gray-700 rounded-lg text-white min-h-30 font-bold cursor-pointer"
               @click="showTaskModal(task)"
             >
               {{ task.name }}
-
-              <!-- 
-              priority if else with 3 icons
-              assigne with avatar if empty leave empty
-              if clicked on task show modal with task details
-              -->
+              <div class="absolute bottom-2 right-2 flex items-center space-x-2">
+                <i v-if="task.priority === 'Low'" class="fas fa-arrow-down text-green-500"></i>
+                <i v-if="task.priority === 'Medium'" class="fas fa-equals text-yellow-500"></i>
+                <i v-if="task.priority === 'High'" class="fas fa-arrow-up text-red-500"></i>
+                <img v-if="task.assignedUser" :src="`http://127.0.0.1:8090/api/files/users/${task.assignedUser.id}/${task.assignedUser.avatar}`" alt="Avatar" class="w-6 h-6 rounded-full" />
+              </div>
             </div>
-          
           </div>
         </div>
       </div>
-      <TaskDetailsModal v-if="showModal" :task="selectedTask" @taskUpdated="handleTaskUpdated":showModal="showModal" :onClose="closeTaskModal" /> 
+    </div>
+
+    <TaskDetailsModal v-if="showModal" :task="selectedTask" @taskUpdated="handleTaskUpdated" :showModal="showModal" :onClose="closeTaskModal" />
   </div>
 </template>
