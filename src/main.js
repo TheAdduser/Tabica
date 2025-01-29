e7c62a6d95e3ccc63775a8f798be1879e031cfca
TheAdduser <EasyRider190@gmail.com>
2025-01-29 19:23:47 +0100

    remove unused libraries

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import dragAndDrop from './directives/dragAndDrop';
 import './style.css';
 
 const app = createApp(App);
-app.directive('drag-and-drop', dragAndDrop);
 app.use(router);
 app.mount('#app');
